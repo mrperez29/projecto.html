660e9536698cb24f2fe7a71a8c4d5e91ef32584f
mrperez29 <angel2005perez29@gmail.com>
2023-10-19 11:00:37 -0500

    mensaje

---
+++ js/categorias.js
@@ -0,0 +1,5 @@
+// Supongamos que esta es la página "categoria1.html"
+document.addEventListener("DOMContentLoaded", function() {
+    // Marca la categoría actual
+    document.getElementById("Artistas").classList.add("activo");
+});
